ee24af49a47e3a91fc077c3016b08ee7ebe21a95
Jilyfe <bouletnicolas31@gmail.com>
2024-09-06 09:03:12 +0400

    Add showcase page

---
+++ src/Controller/ShowcaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShowcaseController extends AbstractController
+{
+    #[Route('/showcase', name: 'app_showcase')]
+    public function index(): Response
+    {
+        return $this->render('showcase/index.html.twig', [
+            'controller_name' => 'ShowcaseController',
+        ]);
+    }
+}
