021df4a5d909dee6fc7cbc9a512f28481ef530a8
Jilyfe <bouletnicolas31@gmail.com>
2024-10-04 09:30:43 +0400

    Manipulate variation with arrows

--- assets/variationboard.js
+++ assets/variationboard.js
@@ -9,13 +9,16 @@ import { Chess } from 'chess.js';
 const chess = new Chess();
 let board;
 
+// moves to be played
+let moves = [];
+
 document.addEventListener('DOMContentLoaded', function () {
     const variation = JSON.parse(document.querySelector('.js-variation').getAttribute('data-variation'));
     console.log(variation);
 
     board = new Chessboard(document.getElementById('board'), {
         position: variation.FEN,
-        assetsUrl: '/cm-chessboard/assets/', // wherever you copied the assets folder to, could also be in the node_modules folder
+        assetsUrl: '/cm-chessboard/assets/',
         extensions: [{ class: Markers }],
         style: {
             pieces: {
@@ -26,35 +29,110 @@ document.addEventListener('DOMContentLoaded', function () {
 
     chess.load(variation.FEN);
 
-    for (let move in variation.moves) {
-        playMove(variation.moves[move].notation.text);
+    for (let moveEntity in variation.moves) {
+        playMove(variation.moves[moveEntity].notation.text, true);
     }
+
+    moves = chess.history().slice().reverse();
+
+    chess.reset();
+
+    document.addEventListener('keydown', keyPressed);
 });
 
-function playMove(notation) {
+function keyPressed(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            undoMove();
+            break;
+        case 'ArrowRight':
+            redoMove();
+            break;
+    }
+}
+
+function playMove(notation, chessOnly = false) {
+
+    /**
+     * Play chess move
+     */
     const move = chess.move(notation);
 
-    board.movePiece(move.from, move.to);
+    if (!chessOnly) {
+        /**
+         * Play board move
+         */
+        board.movePiece(move.from, move.to);
+        switch (true) {
+            case move.flags.includes('k'):
+            case move.flags.includes('q'):
+                let from;
+                let to;
+                if (move.flags.includes('k')) {
+                    from = 'h' + move.from.substring(1);
+                    to = 'f' + move.to.substring(1);
+                } else {
+                    from = 'a' + move.from.substring(1);
+                    to = 'd' + move.to.substring(1);
+                }
+                board.movePiece(from, to);
+                break;
+            case move.flags.includes('e'):
+                board.setPiece(move.to.substring(0, 1) + move.from.substring(1), null);
+                break;
+            case move.flags.includes('p'):
+                board.setPiece(move.to, move.color + move.promotion);
+                break;
+        }
+    }
+}
 
-    switch (true) {
-        case move.flags.includes('k'):
-        case move.flags.includes('q'):
-            let from;
-            let to;
-            if (move.flags.includes('k')) {
-                from = 'h' + move.from.substring(1);
-                to = 'f' + move.to.substring(1);
-            } else {
-                from = 'a' + move.from.substring(1);
-                to = 'd' + move.to.substring(1);
-            }
-            board.movePiece(from, to);
-            break;
-        case move.flags.includes('e'):
-            board.setPiece(move.to.substring(0, 1) + move.from.substring(1), null);
-            break;
-        case move.flags.includes('p'):
-            board.setPiece(move.to, move.color + move.promotion);
-            break;
+function undoMove() {
+
+    /**
+     * Play chess move
+     */
+    const move = chess.undo();
+
+    if (move) {
+
+        /**
+         * Play board move
+         */
+        board.movePiece(move.to, move.from);
+        switch (true) {
+            case move.flags.includes('k'):
+            case move.flags.includes('q'):
+                let from;
+                let to;
+                if (move.flags.includes('k')) {
+                    from = 'h' + move.from.substring(1);
+                    to = 'f' + move.to.substring(1);
+                } else {
+                    from = 'a' + move.from.substring(1);
+                    to = 'd' + move.to.substring(1);
+                }
+                board.movePiece(to, from);
+                break;
+            case move.flags.includes('e'):
+                board.setPiece(move.to.substring(0, 1) + move.from.substring(1), move.color + 'p');
+                break;
+            case move.flags.includes('p'):
+                board.setPiece(move.from, move.color + 'p');
+                break;
+        }
+
+        if (move.flags.includes('c')) {
+            board.setPiece(move.to, (move.color === 'w' ? 'b' : 'w') + move.captured);
+        }
+        
+        moves.push(move);
+    }
+}
+
+function redoMove() {
+    const move = moves.pop();
+    if (move) {
+        playMove(move);
     }
 }
