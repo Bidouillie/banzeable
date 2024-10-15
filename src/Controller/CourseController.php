3d650c906e0031347fbbf93df5fc413f9b29c7d8
Jilyfe <bouletnicolas31@gmail.com>
2024-10-15 11:12:52 +0400

    Link to study course with security

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -14,10 +14,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/course')]
 class CourseController extends AbstractController
 {
-    #[Route('/index', name: 'app_course')]
+    #[Route('/course/index', name: 'app_course')]
     public function index(CourseRepository $repo): Response
     {
         $courses = $repo->findAll();
@@ -28,29 +27,13 @@ class CourseController extends AbstractController
     }
 
     #[IsGranted('IS_AUTHENTICATED')]
-    #[Route('/studies', name: 'app_studies')]
-    public function studies(Security $security): Response
+    #[Route('/course/{id}', name: 'app_course_show', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function show(Request $request, ?Course $course, EntityManagerInterface $em): Response
     {
         /**
          * @var User $user
          */
-        $user = $security->getUser();
-
-        $studies = $user->getStudies();
-
-        return $this->render('course/studies.html.twig', [
-            'studies' => $studies,
-        ]);
-    }
-
-    #[IsGranted('IS_AUTHENTICATED')]
-    #[Route('/{id}', name: 'app_course_show', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    public function show(Request $request, Security $security, ?Course $course, EntityManagerInterface $em): Response
-    {
-        /**
-         * @var User $user
-         */
-        $user = $security->getUser();
+        $user = $this->getUser();
 
         $form = $this->createForm(StudyToggleType::class, [
             'action' => $course ? ($user->getStudies()->contains($course) ? 'remove' : 'add') : '',
@@ -75,6 +58,33 @@ class CourseController extends AbstractController
         return $this->render('course/show.html.twig', [
             'course' => $course,
             'form' => $form,
+        ]);
+    }
+
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/studies', name: 'app_studies')]
+    public function studies(Security $security): Response
+    {
+        /**
+         * @var User $user
+         */
+        $user = $security->getUser();
+
+        $studies = $user->getStudies();
+
+        return $this->render('course/studies.html.twig', [
+            'studies' => $studies,
+        ]);
+    }
+
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/study/{id}', name: 'app_study', requirements: ['id' => '\d+'])]
+    public function study(?Course $course): Response
+    {
+        $this->denyAccessUnlessGranted('course.studies', $course);
+
+        return $this->render('course/study.html.twig', [
+            'course' => $course,
         ]);
     }
 }
