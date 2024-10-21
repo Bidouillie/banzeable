cb3d3f6eba761f85d2ef59245492f46864c7cbb4
Jilyfe <bouletnicolas31@gmail.com>
2024-10-21 17:28:59 +0400

    Study progress bar

--- assets/studyboard.js
+++ assets/studyboard.js
@@ -2,6 +2,7 @@
 import { ChessboardEngine } from './chessboardengine/chessboardengine.js'
 import { ToastMaker } from './toast.js';
 
+// ChessboardEngine
 let board;
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -12,15 +13,31 @@ document.addEventListener('DOMContentLoaded', function () {
         return move.notation.text;
     }), variation.blackOrientation ? 'b' : 'w');
 
+    let movesDone = 0, totalMoves = Math.floor((variation.moves.length + (variation.blackOrientation ? 0 : 1)) / 2);
+
+    const progressBar = document.getElementById('progressBar');
+
     board.enablePlayableMove((event) => {
         if (event.moveValidation) {
-            if (event.lastMove) {
-                ToastMaker.toast('Good move, variation is over!', 'success', 3000);
-            } else {
-                ToastMaker.toast('Good move', 'success');
+
+            movesDone++;
+            for (let i = progressBar.classList.length - 1; i >= 0; i--) {
+                const className = progressBar.classList[i];
+                if (className.startsWith('bg-')) {
+                    progressBar.classList.remove(className);
+                }
             }
+            if (event.lastMove) {
+                ToastMaker.toast("Good move, variation is over!", 'success', 3000);
+                progressBar.classList.remove('progress-bar-animated');
+                progressBar.classList.remove('progress-bar-striped');
+                progressBar.classList.add('bg-success');
+            } else {
+                ToastMaker.toast("Good move", 'success');
+            }
+            progressBar.style.width = Math.floor(movesDone * 100 / totalMoves).toString() + "%";
         } else {
-            ToastMaker.toast('Wrong move', 'danger', 2000);
+            ToastMaker.toast("Wrong move", 'danger');
         }
     }, 1000, 'variation');
 
